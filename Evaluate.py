774a9191ea019605ba093748738426610b01b7d0
Muhammad Irfan Danish <irdanish11@gmail.com>
2020-02-22 23:31:52 +0500

    added files

---
+++ Evaluate.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 13:17:22 2020
+
+@author: danish
+"""
+
+import pandas as pd
+from tensorflow.keras.applications.vgg16 import VGG16
+from UCF_Model import UCFModel, PredictAction, PredictOnBatch
+from VideoPreprocessing import VideoNameDF
+from sklearn.metrics import accuracy_score
+
+
+path = 'D:/Study/DataScience/DataSets/UCF'
+
+base_model = VGG16(weights='imagenet', include_top=False)
+model = UCFModel(shape=25088)
+# loading the trained weights
+model.load_weights(path+'/ckpt/UCF_weights.hdf5')
+# compiling the model
+model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
+
+# getting the test list
+test =  VideoNameDF(name='testlist01.txt', dir='ucfTrainTestlist')
+test_videos = test['video_name']
+
+# creating the tags
+train = pd.read_csv('UCF/train_new.csv')
+y = train['class']
+y = pd.get_dummies(y)
+
+
+################# Making a prediction on single video ################
+file='UCF/v_Archery_g03_c03.avi'
+action = PredictAction(file, y, model, base_model)
+print(action)
+    
+################# Making a prediction on batch of videos ################
+predict, actual = PredictOnBatch(test_videos, y, base_model, model, videos_dir=path+'/UCF-101')
---
+++ ucf101.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 17:27:58 2020
+@author: danish
+Dependencies:
+    conda install -c conda-forge opencv
+    pip install scikit-image
+    pip install tqdm 
+"""
+
+from VideoPreprocessing import VideoNameDF, TagVideos, Video2Frames, FramesCSV
+from UCF_Model import ReadFrames, DatasetSplit, VGG16Model, UCFModel_Train
+import tqdm
+from keras.preprocessing import image
+import numpy as np
+
+
+###################### Video Preprocessing ########################
+# creating a dataframe having video names
+train = VideoNameDF(name='trainlist01.txt', dir='ucfTrainTestlist')
+test =  VideoNameDF(name='testlist01.txt', dir='ucfTrainTestlist')
+
+#Next, we will add the tag of each video (for both training and test sets).
+train = TagVideos(train)
+test = TagVideos(test)
+
+#Extracting frames from training videos.
+path = 'D:/Study/DataScience/DataSets/UCF'
+status = Video2Frames(train, frames_dir=path+'/train_1', videos_dir=path+'/UCF-101')
+print(status)
+
+#Save the names of the frames to a CSV file along with their corresponding tags.
+train_data = FramesCSV(frames_dir=path+'/train_1', csv_dir='UCF', csv_name='train_new.csv')
+
+################### Data Preprocessing ##########################    
+
+import pandas as pd
+train = pd.read_csv('UCF/train_new.csv')
+train.head()
+
+X = ReadFrames(train, frames_dir=path+'/train_1')
+#Creating the test set and validation set.
+X_train, X_test, y_train, y_test = DatasetSplit(train, X)
+
+################### Train the Model ##########################
+X_train, X_test = VGG16Model(X_train, X_test)
+history = UCFModel_Train(X_train, y_train, X_test, y_test, epochs=200, ckpt_name='UCF_weights.h5')
